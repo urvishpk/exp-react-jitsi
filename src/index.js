dc565da421e7f5c05b464c8669389dc4ad3d2660
urvishkathiriya <urvish.kathiriya@gmail.com>
2020-10-23 01:06:13 +0530

    Quicksave

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App";
-import OldApp from "./OldApp";
 import NewApp from "./NewApp";
 import * as serviceWorker from "./serviceWorker";
 
